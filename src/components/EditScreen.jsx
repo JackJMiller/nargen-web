48782b010db8f737756b14cdf1edf4155ccb53cb
JackJMiller <jackjamesmiller.dev@gmail.com>
2025-06-09 12:28:27 +0100

    introduce 'panes' state in the edit screen

--- src/components/EditScreen.jsx
+++ src/components/EditScreen.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Draggable from "./Draggable";
 import BiomePane from "./BiomePane";
 import SubBiomePane from "./SubBiomePane";
@@ -5,19 +7,38 @@ import WorldPane from "./WorldPane";
 
 function EditScreen() {
 
+    let [panes, setPanes] = useState([
+        { type: "world", name: "World Config" },
+        { type: "biome", name: "rainvalley" },
+        { type: "sub-biome", name: "rainvalley.clusters" }
+    ]);
+
+    let renderPane = (pane) => {
+        switch (pane.type) {
+            case "world":
+                return (
+                    <Draggable x={ 0.1 * window.innerWidth } y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
+                        <WorldPane name={ pane.name }/>
+                    </Draggable>
+                );
+            case "biome":
+                return (
+                    <Draggable x={ 0.3 * window.innerWidth } y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
+                        <BiomePane name={ pane.name }/>
+                    </Draggable>
+                );
+            case "sub-biome":
+                return (
+                    <Draggable x={ 0.5 * window.innerWidth } y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
+                        <SubBiomePane name={ pane.name }/>
+                    </Draggable>
+                );
+        }
+    };
+
     return (
         <>
-            <Draggable x={ 0.1 * window.innerWidth} y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
-                <WorldPane name="World Config"/>
-            </Draggable>
-
-            <Draggable x={ 0.3 * window.innerWidth } y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
-                <BiomePane name="rainvalley"/>
-            </Draggable>
-
-            <Draggable x={ 0.5 * window.innerWidth } y={ 0.6 * window.innerHeight } dragBoxClassName="window-header">
-                <SubBiomePane name="rainvalley.clusters"/>
-            </Draggable>
+            { panes.map((pane) => renderPane(pane)) }
         </>
     );
 
