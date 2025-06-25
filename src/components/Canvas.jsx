7845f11ebeddee4be4878c301a749c51aeab0b03
JackJMiller <jackjamesmiller.dev@gmail.com>
2025-06-25 12:36:56 +0100

    delete Canvas component

--- src/components/Canvas.jsx
+++
@@ -1,8 +0,0 @@
-import { RENDERER } from "../scripts/env_script";
-import "../styles/Canvas.css";
-
-function Canvas() {
-    return <canvas className="chunk-canvas" id="game-canvas" width="150" height="150"></canvas>
-}
-
-export default Canvas;
